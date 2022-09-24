495417aa80e21f16804b44947e42490f69974dfc
Narasimhulu <98338387+Narasimhuluboddeti@users.noreply.github.com>
2022-09-24 14:39:09 +0530

    #include <bits/stdc++.h> using namespace std; int main(){     int a;     cin>>a;     if(a>0){         cout<<"Positive Number";     }     else{         cout<<"Negative Number";     }     return 0; }

---
+++ Program_to_check_whether_the_given_integer_is_positive_or_negative.c
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a;
+    cin>>a;
+    if(a>0){
+        cout<<"Positive Number";
+    }
+    else{
+        cout<<"Negative Number";
+    }
+    return 0;
+}
