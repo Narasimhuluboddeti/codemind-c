846d5bcd32b72e0a40f5d38e7b4e12629a90ef9e
Narasimhulu <98338387+Narasimhuluboddeti@users.noreply.github.com>
2022-10-04 15:07:08 +0530

    #include<bits/stdc++.h> using namespace std; int main(){     string a;     cin>>a;     if(a.size()==10 or a[0]==9 or a[0]==8){         cout<<"Valid";     }     else{         cout<<"Invalid";     } }

---
+++ PHONE_NUMBER_VALIDATION:.c
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string a;
+    cin>>a;
+    if(a.size()==10 or a[0]==9 or a[0]==8){
+        cout<<"Valid";
+    }
+    else{
+        cout<<"Invalid";
+    }
+}
